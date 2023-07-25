b05a638e020fcaf4e76a749934591e2253815e9f
Tommi Mäklin <tommi@maklin.fi>
2023-07-25 14:06:06 +0100

    Wrap the zlib calls.

--- include/tigz_decompressor.hpp
+++ include/tigz_decompressor.hpp
@@ -45,6 +45,101 @@
 #include "BS_thread_pool.hpp"
 
 namespace tigz {
+namespace zwrapper {
+    int init_z_stream(z_stream *strm_p, const size_t window_size = 0) {
+	strm_p->zalloc = Z_NULL;
+	strm_p->zfree = Z_NULL;
+	strm_p->opaque = Z_NULL;
+	strm_p->avail_in = 0;
+	strm_p->next_in = Z_NULL;
+	int ret = (window_size == 0 ? inflateInit(strm_p) : inflateInit2(strm_p, window_size));
+	return ret;
+    }
+
+    int decompress_block(const size_t chunk_size, std::basic_string<unsigned char> *out, std::ostream *dest, z_stream *strm_p) {
+	int ret; // zlib return code
+
+	do {
+	    strm_p->avail_out = chunk_size;
+	    strm_p->next_out = out->data();
+	    ret = inflate(strm_p, Z_NO_FLUSH);
+	    switch (ret) {
+	    case Z_STREAM_ERROR:
+		(void)inflateEnd(strm_p);
+		return ret;
+	    case Z_NEED_DICT:
+		ret = Z_DATA_ERROR;     /* and fall through */
+	    case Z_DATA_ERROR:
+	    case Z_MEM_ERROR:
+		(void)inflateEnd(strm_p);
+		return ret;
+	    }
+	    size_t have = chunk_size - strm_p->avail_out;
+	    dest->write(reinterpret_cast<char*>(out->data()), have);
+	    if (dest->fail()) {
+		(void)inflateEnd(strm_p);
+		return Z_ERRNO;
+	    }
+	} while (strm_p->avail_out == 0);
+
+	return ret;
+    }
+
+    // Default to zlib when decompressing from an unseekable stream
+    int decompress_stream(const size_t chunk_size, std::istream *source, std::ostream *dest) {
+	// Adapted from the zlib usage examples
+	// https://www.zlib.net/zlib_how.html
+	std::basic_string<unsigned char> in(chunk_size, '-');
+	std::basic_string<unsigned char> out(chunk_size, '-');
+
+	/* allocate inflate state */
+	z_stream strm;
+	int ret = init_z_stream(&strm, 15+32);
+
+	if (ret != Z_OK)
+	    return ret;
+
+	while (source->good()) {
+
+	    /* decompress until deflate stream ends or end of file */
+	    do {
+		source->read(reinterpret_cast<char*>(in.data()), chunk_size);
+		strm.avail_in = source->gcount();
+		if (source->fail() && !source->eof()) {
+		    (void)inflateEnd(&strm);
+		    return Z_ERRNO;
+		}
+		if (strm.avail_in == 0)
+		    break;
+		strm.next_in = in.data();
+
+		/* run inflate() on input until output buffer not full */
+		ret = decompress_block(chunk_size, &out, dest, &strm);
+
+		// Part done when ret == Z_STREAM_END
+	    } while (ret != Z_STREAM_END);
+	    if (source->good()) {
+		// Probably reading a multipart file, keep going
+		// Previous inflate() may not have consumed the entire input buffer
+		int in_to_consume = chunk_size - (strm.next_in - &in.data()[0]); // Pointer arithmetic to determine amount of buffer left
+		unsigned char* header_start = &in.data()[chunk_size - strm.avail_in];
+		(void)inflateEnd(&strm);
+		ret = init_z_stream(&strm, 15+32);
+		strm.next_in = header_start;
+		strm.avail_in = in_to_consume;
+
+		/* run inflate() on input until output buffer not full */
+		decompress_block(chunk_size, &out, dest, &strm);
+	    } else {
+		// Done
+		(void)inflateEnd(&strm);
+	    }
+	}
+
+	return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+    }
+}
+
 class ParallelDecompressor {
 private:
     // Rapidgzip toggles
@@ -56,133 +151,6 @@ private:
 
     // Number of threads to use in file decompression
     size_t n_threads;
-
-    // Default to zlib when decompressing from an unseekable stream
-    int decompress_with_zlib(std::istream *source, std::ostream *dest) const {
-	// Adapted from the zlib usage examples
-	// https://www.zlib.net/zlib_how.html
-	std::basic_string<unsigned char> in(this->chunkSize, '-');
-	std::basic_string<unsigned char> out(this->chunkSize, '-');
-
-
-	int ret;
-	/* allocate inflate state */
-	z_stream strm;
-	strm.zalloc = Z_NULL;
-	strm.zfree = Z_NULL;
-	strm.opaque = Z_NULL;
-	strm.avail_in = 0;
-	strm.next_in = Z_NULL;
-	ret = inflateInit2(&strm, 15+32);
-	if (ret != Z_OK)
-	    return ret;
-
-	while (source->good()) {
-
-	    /* decompress until deflate stream ends or end of file */
-	    do {
-		source->read(reinterpret_cast<char*>(in.data()), this->chunkSize);
-		strm.avail_in = source->gcount();
-		if (source->fail() && !source->eof()) {
-		    (void)inflateEnd(&strm);
-		    return Z_ERRNO;
-		}
-		if (strm.avail_in == 0)
-		    break;
-		strm.next_in = in.data();
-
-		/* run inflate() on input until output buffer not full */
-		do {//
-		    strm.avail_out = this->chunkSize;
-		    strm.next_out = out.data();
-		    ret = inflate(&strm, Z_NO_FLUSH);
-		    switch (ret) {
-		    case Z_STREAM_ERROR:
-			(void)inflateEnd(&strm);
-			return ret;
-		    case Z_NEED_DICT:
-			ret = Z_DATA_ERROR;     /* and fall through */
-		    case Z_DATA_ERROR:
-		    case Z_MEM_ERROR:
-			(void)inflateEnd(&strm);
-			return ret;
-		    }
-		    size_t have = this->chunkSize - strm.avail_out;
-		    dest->write(reinterpret_cast<char*>(out.data()), have);
-		    if (dest->fail()) {
-			(void)inflateEnd(&strm);
-			return Z_ERRNO;
-		    }
-		} while (strm.avail_out == 0);
-		// Part done when ret == Z_STREAM_END
-	    } while (ret != Z_STREAM_END);
-	    if (source->good()) {
-		// Probably reading a multipart file, keep going
-		// Previous inflate() may not have consumed the entire input buffer
-		int in_to_consume = this->chunkSize - (strm.next_in - &in.data()[0]); // Pointer arithmetic to determine amount of buffer left
-		unsigned char* header_start = &in.data()[this->chunkSize - strm.avail_in];
-		(void)inflateEnd(&strm);
-		strm.zalloc = Z_NULL;
-		strm.zfree = Z_NULL;
-		strm.opaque = Z_NULL;
-		strm.avail_in = 0;
-		strm.next_in = Z_NULL;
-		ret = inflateInit2(&strm, 15+32);
-		strm.next_in = header_start;
-		strm.avail_in = in_to_consume;
-		/* run inflate() on input until output buffer not full */
-		do {//
-		    strm.avail_out = this->chunkSize;
-		    strm.next_out = out.data();
-		    ret = inflate(&strm, Z_NO_FLUSH);
-		    switch (ret) {
-		    case Z_STREAM_ERROR:
-			(void)inflateEnd(&strm);
-			return ret;
-		    case Z_NEED_DICT:
-			ret = Z_DATA_ERROR;     /* and fall through */
-		    case Z_DATA_ERROR:
-		    case Z_MEM_ERROR:
-			(void)inflateEnd(&strm);
-			return ret;
-		    }
-		    size_t have = this->chunkSize - strm.avail_out;
-		    dest->write(reinterpret_cast<char*>(out.data()), have);
-		    if (dest->fail()) {
-			(void)inflateEnd(&strm);
-			return Z_ERRNO;
-		    }
-		} while (strm.avail_out == 0);
-	    } else {
-		// Done
-		(void)inflateEnd(&strm);
-	    }
-	}
-
-	return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-    }
-
-    // Single-threaded decompression with rapidgzip
-    size_t decompress_with_single_thread(UniqueFileReader &inputFile, std::unique_ptr<OutputFile> &output_file) const {
-	const auto outputFileDescriptor = output_file ? output_file->fd() : -1;
-	size_t newlineCount = 0;
-	const auto writeAndCount =
-	    [outputFileDescriptor, &newlineCount, this]
-	    (const void* const buffer,
-	     uint64_t const size) {
-		if (outputFileDescriptor >= 0) {
-		    writeAllToFd(outputFileDescriptor, buffer, size);
-		}
-		if (this->countLines) {
-		    newlineCount += countNewlines({reinterpret_cast<const char*>(buffer),
-			    static_cast<size_t>(size )});
-		}
-	    };
-	rapidgzip::GzipReader gzipReader{ std::move(inputFile) };
-	gzipReader.setCRC32Enabled( this->crc32Enabled );
-	size_t totalBytesRead = gzipReader.read(writeAndCount);
-	return totalBytesRead;
-    }
 
     // Multithreaded decompression with rapidgzip
     size_t decompress_with_many_threads(UniqueFileReader &inputFile, std::unique_ptr<OutputFile> &output_file) const {
@@ -218,17 +186,8 @@ public:
 	this->n_threads = _n_threads;
     }
 
-    ~ParallelDecompressor() {
-    }
-
-    // Delete copy and move constructors & copy and move assignment operators
-    ParallelDecompressor(const ParallelDecompressor& other) = delete;
-    ParallelDecompressor(ParallelDecompressor&& other) = delete;
-    ParallelDecompressor& operator=(const ParallelDecompressor& other) = delete;
-    ParallelDecompressor& operator=(const ParallelDecompressor&& other) = delete;
-
     void decompress_stream(std::istream *in, std::ostream *out) const {
-	this->decompress_with_zlib(in, out);
+	zwrapper::decompress_stream(this->chunkSize, in, out);
     }
 
     void decompress_file(const std::string &in_path, std::string &out_path) const {
@@ -236,10 +195,10 @@ public:
         if (this->n_threads == 1) {
 	    std::ifstream in(in_path);
 	    if (out_path.empty()) {
-		this->decompress_with_zlib(&in, &std::cout);
+		zwrapper::decompress_stream(this->chunkSize, &in, &std::cout);
 	    } else {
 		std::ofstream out(out_path);
-		this->decompress_with_zlib(&in, &out);
+		zwrapper::decompress_stream(this->chunkSize, &in, &out);
 		out.close();
 	    }
 	    in.close();
